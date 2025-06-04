a5ac8642f76b76a6d5efb7bd110636af2026acc3
romosken <rodrigo.mosken@outlook.com>
2025-06-04 15:36:55 -0300

    feat: adding document validation to customer form

--- customers/views.py
+++ customers/views.py
@@ -6,11 +6,13 @@ from django.http import HttpResponse
 
 from customers.models import Car, Customer
 
+
 def customers(request):
     if request.method == "GET":
         return render(request, 'customers.html')
     elif request.method == "POST":
         save_customer(request)
+
 
 def save_customer(request):
     name = request.POST.get('name')
@@ -22,23 +24,25 @@ def save_customer(request):
     car_plates = request.POST.getlist('car-plate')
     car_years = request.POST.getlist('car-year')
 
+    # TODO overwrite the form with ModelForm from django
+
+    db_customer = Customer.objects.filter(document=document)
+    if db_customer.exists():
+        return render(request, "customers.html", {"name": name, "surname": surname, "email": email, "cars": zip(car_models, car_plates, car_years)})
+
     customer = Customer(
-            name=name, 
-            surname=surname,
-            email=email, 
-            document=document
-        )
-    customer.validate_constraints() #TODO overwrite the form with ModelForm from django
-    try:
-        customer.save()
-    except IntegrityError as e:
-        print(e.__str__())
-        render
+        name=name,
+        surname=surname,
+        email=email,
+        document=document
+    )
+
+    customer.save()
 
     for index in range(len(car_models)):
         Car(
-                model= car_models[index],
-                plate= car_plates[index],
-                year= car_years[index],
-                customer=customer
-            ).save()
+            model=car_models[index],
+            plate=car_plates[index],
+            year=car_years[index],
+            customer=customer
+        ).save()
